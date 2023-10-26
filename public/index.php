0aea0d5c7cd44452fe1591a78e2489485f52fb7f
CiprianoDev <cilg011022@itsuruapan.edu.mx>
2023-10-25 20:29:06 -0600

    Feature: Add login page
    This commit introduces the login page into the project, allowing users to authenticate and access their accounts. The following features have been implemented:
    
    - Login page
    - Credential validation
    - Secure password storage

--- public/index.php
+++ public/index.php
@@ -2,12 +2,13 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\LoginController;
 use MVC\Router;
 $router = new Router();
 
 
 //Login
-
+$router->get('/', [LoginController::class,'index']);
 
 // Check and validate the routes, ensuring their existence and assigning them the Controller functions
 $router->checkRoutes();
---
+++ views/auth/login.php
@@ -0,0 +1,20 @@
+<main class="main-container">
+    <div class="image">
+        <img src="/build/img/logoGestion.png" alt="Logo Tec">
+    </div>
+    <div class="login">
+        <form action="post" class="form">
+            <label class="title">Iniciar Sesión</label>
+            <div class="field">
+                <label for="correo">Correo Electrónico:</label>
+                <input type="email">
+            </div>
+            <div class="field">
+                <label for="password">Contraseña:</label>
+                <input type="password">
+            </div>
+
+            <input type="submit" class="button" value="Iniciar Sesión">
+        </form>
+    </div>
+</main>
